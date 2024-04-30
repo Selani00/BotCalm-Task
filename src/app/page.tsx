5a13e4523dff7ab4371d69f1eaa1ebac92d16b71
Selani00 <pkselani00@gmail.com>
2024-04-30 20:37:15 +0530

    Add try,catch

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,39 @@
-'use client'
+"use client";
 import Image from "next/image";
 import Link from "next/link";
 import { useState } from "react";
 
 export default function Home() {
-
   const [userList, setUserList] = useState([]);
 
-    const fetchDate = async () =>{
-      const userslist= await fetch("https://jsonplaceholder.typicode.com/users")
+  const fetchDate = async () => {
+    try {
+      const userslist = await fetch(
+        "https://jsonplaceholder.typicode.com/users"
+      );
       const data = await userslist.json();
       setUserList(data);
+    } catch (e) {
+      console.log(e);
     }
+  };
 
-   
-
-    const handleClick = () =>{
-      console.log("This is serveraction printed on server console")
+  const handleClick = () => {
+    try{
+      console.log("This is serveraction printed on server console");
+    }catch(e){
+      console.log(e)
     }
+  };
   return (
     <div className="grid place-items-center mt-10">
-      <button className="bg-[white] text-black p-5" onClick={handleClick}>Do Server Action</button>
+      <button className="bg-[white] text-black p-5" onClick={handleClick}>
+        Do Server Action
+      </button>
 
-      <button className="mt-10 bg-[white] text-black p-5" onClick={fetchDate}>Get users data</button>
-
+      <button className="mt-10 bg-[white] text-black p-5" onClick={fetchDate}>
+        Get users data
+      </button>
 
       <table className="mt-10 border-collapse border border-gray-800">
         <thead>
@@ -35,13 +45,14 @@ export default function Home() {
         <tbody>
           {userList.map((user) => (
             <tr key={user.id}>
-              <td className="border border-gray-800 py-1 px-3"><Link href={`posts/${user.id}`}>{user.id}</Link></td>
+              <td className="border border-gray-800 py-1 px-3">
+                <Link href={`posts/${user.id}`}>{user.id}</Link>
+              </td>
               <td className="border border-gray-800 py-1 px-3">{user.name}</td>
             </tr>
           ))}
         </tbody>
       </table>
-      
     </div>
   );
 }
