f7cd421191e93c107d8db3c14430286a6e8421a7
Selani00 <pkselani00@gmail.com>
2024-04-30 19:00:01 +0530

    Create a table with users data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
 'use client'
 import Image from "next/image";
+import { use, useEffect,useState } from "react";
 
 export default function Home() {
-    
+
+  const [userList, setUserList] = useState([]);
+
+    const fetchDate = async () =>{
+      const userslist= await fetch("https://jsonplaceholder.typicode.com/users")
+      const data = await userslist.json();
+      setUserList(data);
+    }
+
+    // useEffect(() => {
+    //   fetchDate();
+    // }, [])
+
     const handleClick = () =>{
       console.log("This is serveraction printed on server console")
     }
@@ -10,10 +23,25 @@ export default function Home() {
     <div className="grid place-items-center mt-10">
       <button className="bg-[white] text-black p-5" onClick={handleClick}>Do Server Action</button>
 
+      <button className="mt-10 bg-[white] text-black p-5" onClick={fetchDate}>Get users data</button>
 
-      <div className="usersdata">
-        
-      </div>
+
+      <table className="mt-10 border-collapse border border-gray-800">
+        <thead>
+          <tr>
+            <th className="border border-gray-800 py-1 px-3">ID</th>
+            <th className="border border-gray-800 py-1 px-3">Name</th>
+          </tr>
+        </thead>
+        <tbody>
+          {userList.map((user) => (
+            <tr key={user.id}>
+              <td className="border border-gray-800 py-1 px-3">{user.id}</td>
+              <td className="border border-gray-800 py-1 px-3">{user.name}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
